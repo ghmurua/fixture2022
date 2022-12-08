04f2df4606ee6153929d1adeb9ae143dfa042136
Gabriel Murua <gmuruadev@gmail.com>
2022-12-07 22:40:09 -0300

    Mostrando partidos fase de grupos

--- code.js
+++ code.js
@@ -1,16 +1,37 @@
-const divGroups = document.querySelector('.divGroups')
+// mostrando en pantalla los grupos
+const divGroups = document.querySelector(".divGroups");
 
-dataGroups.map(group => {
-    let div = document.createElement('DIV')
-    let header = document.createElement('HEADER')
-    header.innerText = group[0]
-    div.appendChild(header)
+dataGroups.map((group) => {
+  let div = document.createElement("DIV");
+  let header = document.createElement("HEADER");
+  header.innerText = group[0];
+  div.appendChild(header);
 
-    for ( let i=1; i<group.length; i++) {
-        let p = document.createElement('P')
-        p.innerText =  group[i]
-        div.appendChild(p)
-    }
+  for (let i = 1; i < group.length; i++) {
+    let p = document.createElement("P");
+    p.innerText = group[i];
+    div.appendChild(p);
+  }
 
-    divGroups.appendChild(div)
-})
+  divGroups.appendChild(div);
+});
+
+// mostrando los partidos de fase de grupos
+const divMatches = document.querySelector(".divMatches");
+const matchDates = Object.keys(dataGroupMatches); // ['nov20','nov21'...]
+
+for (let i = 0; i < matchDates.length; i++) {
+  let div = document.createElement("DIV");
+  let h2 = document.createElement("H2");
+  h2.innerText = `${matchDates[i]}`;
+  div.appendChild(h2);
+
+  for (let j = 0; j < dataGroupMatches[matchDates[i]].length; j++) {
+    let info = dataGroupMatches[matchDates[i]][j];
+    let p = document.createElement("p");
+    p.innerText = `${info[0]}: ${info[1]} vs ${info[2]}`;
+    div.appendChild(p);
+  }
+
+  divMatches.appendChild(div);
+}
--- data.js
+++ data.js
@@ -1,10 +1,85 @@
 const dataGroups = [
-    ["A", "Qatar", "Ecuador", "Senegal", "Paises Bajos"],
-    ["B", "Inglaterra", "Irán", "Estados Unidos", "Gales"],
-    ["C", "Argentina", "Arabia Saudita", "México", "Polonia"],
-    ["D", "Francia", "Australia", "Dinamarca", "Túnez"],
-    ["E", "España", "Costa Rica", "Alemania", "Japón"],
-    ["F", "Bélgica", "Canadá", "Marruecos", "Croacia"],
-    ["G", "Brasil", "Serbia", "Suiza", "Camerún"],
-    ["H", "Portugal", "Ghana", "Uruguay", "Corea del Sur"]
+  ["A", "Qatar", "Ecuador", "Senegal", "Paises Bajos"],
+  ["B", "Inglaterra", "Irán", "Estados Unidos", "Gales"],
+  ["C", "Argentina", "Arabia Saudita", "México", "Polonia"],
+  ["D", "Francia", "Australia", "Dinamarca", "Túnez"],
+  ["E", "España", "Costa Rica", "Alemania", "Japón"],
+  ["F", "Bélgica", "Canadá", "Marruecos", "Croacia"],
+  ["G", "Brasil", "Serbia", "Suiza", "Camerún"],
+  ["H", "Portugal", "Ghana", "Uruguay", "Corea del Sur"],
 ];
+
+const dataGroupMatches = {
+  "Nov 20": [["19:00", "Qatar", "Ecuador", "A"]],
+  "Nov 21": [
+    ["16:00", "Senegal", "Paises Bajos", "A"],
+    ["19:00", "Inglaterra", "Irán", "B"],
+    ["22:00", "Estados Unidos", "Gales", "B"],
+  ],
+  "Nov 22": [
+    ["13:00", "Argentina", "Arabia Saudita", "C"],
+    ["16:00", "Dinamarca", "Túnez", "D"],
+    ["19:00", "México", "Polonia", "C"],
+    ["22:00", "Francia", "Australia", "D"],
+  ],
+  "Nov 23": [
+    ["13:00", "Marruecos", "Croacia", "F"],
+    ["16:00", "Alemania", "Japón", "E"],
+    ["19:00", "España", "Costa Rica", "E"],
+    ["22:00", "Bélgica", "Canadá", "F"],
+  ],
+  "Nov 24": [
+    ["13:00", "Suiza", "Camerún", "G"],
+    ["16:00", "Uruguay", "Corea del Sur", "H"],
+    ["19:00", "Portugal", "Ghana", "H"],
+    ["22:00", "Brasil", "Serbia", "G"],
+  ],
+  "Nov 25": [
+    ["13:00", "Gales", "Irán", "B"],
+    ["16:00", "Qatar", "Senegal", "A"],
+    ["19:00", "Paises Bajos", "Ecuador", "A"],
+    ["22:00", "Inglaterra", "Estados Unidos", "B"],
+  ],
+  "Nov 26": [
+    ["13:00", "Túnez", "Australia", "D"],
+    ["16:00", "Polonia", "Arabia Saudita", "C"],
+    ["19:00", "Francia", "Dinamarca", "D"],
+    ["22:00", "Argentina", "México", "C"],
+  ],
+  "Nov 27": [
+    ["13:00", "Japón", "Costa Rica", "E"],
+    ["16:00", "Bélgica", "Marruecos", "F"],
+    ["19:00", "Croacia", "Canadá", "F"],
+    ["22:00", "España", "Alemania", "E"],
+  ],
+  "Nov 28": [
+    ["13:00", "Camerún", "Serbia", "G"],
+    ["16:00", "Corea del Sur", "Ghana", "H"],
+    ["19:00", "Brasil", "Suiza", "G"],
+    ["22:00", "Portugal", "Uruguay", "H"],
+  ],
+  "Nov 29": [
+    ["18:00", "Paises Bajos", "Qatar", "A"],
+    ["18:00", "Ecuador", "Senegal", "A"],
+    ["22:00", "Irán", "Estados Unidos", "B"],
+    ["22:00", "Gales", "Inglaterra", "B"],
+  ],
+  "Nov 30": [
+    ["18:00", "Túnez", "Francia", "D"],
+    ["18:00", "Australia", "Dinamarca", "D"],
+    ["22:00", "Arabia Saudita", "México", "C"],
+    ["22:00", "Polonia", "Argentina", "C"],
+  ],
+  "Dec 1": [
+    ["18:00", "Canadá", "Marruecos", "F"],
+    ["18:00", "Croacia", "Bélgica", "F"],
+    ["22:00", "Costa Rica", "Alemania", "E"],
+    ["22:00", "Japón", "España", "E"],
+  ],
+  "Dec 2": [
+    ["18:00", "Corea del Sur", "Portugal", "H"],
+    ["18:00", "Ghana", "Uruguay", "H"],
+    ["22:00", "Camerún", "Brasil", "G"],
+    ["22:00", "Serbia", "Suiza", "G"],
+  ],
+};
